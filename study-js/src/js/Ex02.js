f03440c5359673064659d1989d98738dd2bb4fd1
ChoiMari <chl98639863@gmail.com>
2025-10-07 18:02:12 +0900

    switch문 - java와 같음

--- study-js/src/js/Ex02.js
+++ study-js/src/js/Ex02.js
@@ -27,3 +27,36 @@ let number = a ?? b;
 console.log(number); //20
 
 //조건문
+//if문
+let num3 = 111;
+let num4 = 222;
+if (num4 > num3) {
+  console.log(`${num4}은 ${num3}보다 크다`);
+  //222은 111보다 크다
+}
+
+//switch case
+let fruit = "사과";
+switch (fruit) {
+  case "배":
+    console.log("배가 맛있다");
+    break;
+  case "사과":
+    console.log("사과가 맛있다"); // 실행됨
+    break;
+  default:
+    console.log("다른 과일이 맛있다");
+}
+
+//switch문은 내부적으로 === (엄격한 비교, strict equality)를 사용
+
+let num5 = 10;
+
+switch (num5) {
+  case "10":
+    console.log("문자열 10"); //실행 안 됨 (타입 다름)
+    break;
+  case 10:
+    console.log("숫자 10"); //실행됨
+    break;
+}
